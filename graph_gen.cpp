2bf36c0aac4de77e3c515a1757379023fca24813
Juraj Machac <jurajmchc@gmail.com>
2016-04-12 01:26:32 +0200

    Generalized, entwined not yet implemented

--- graph_gen.cpp
+++ graph_gen.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <set>
 #include <map>
+#include <vector>
 
 using namespace std;
 
@@ -18,15 +19,18 @@ using namespace std;
 class Vertex {
     public:
         int type;
+        string vtype;
         set<pair<Vertex*, double> > incoming;
         set<pair<Vertex*, double> > outgoing;
-        Vertex(int t) : type(t) {}
+        Vertex(int t, string vt) : type(t), vtype(vt) {} 
+        Vertex(int t) : type(t) {
+            this->vtype = "P";
+        }
         Vertex(){}
         string to_string() const {
-            if (this->incoming.size() == 0) return "";
-            string ret = std::to_string(type) + " " + std::to_string(this->incoming.size()) + "\n";
+            string ret = vtype + " " + std::to_string(type) + " " + std::to_string(this->incoming.size()) + "\n";
             for (auto it : this->incoming) {
-                ret += std::to_string(it.first->type) + " " + std::to_string(it.second) + "\n";
+                ret += it.first->vtype + " " + std::to_string(it.first->type) + " " + std::to_string(it.second) + "\n";
             }
             return ret;
         }
@@ -57,18 +61,24 @@ double rand_df_coef(int max_factor, double prob_of_small) {
     return (should_invert ? 1/on_interval : on_interval);
 }
 
+// FIXME !!
 string gen_rand_entwined(int height, int width, int sdata, int max_factor, double prob_of_small) {
-    string ret = std::to_string((height + 1)*2);
+    string ret = std::to_string((height + 1)*2 + 2);
     ret += " " + std::to_string(sdata) + "\n";
+    ret += "I -1 0\n";
+    ret += "G 0 1\n";
+    ret += "I -1 1";
     for (int i = 0; i <= height; i++) {
         for (int current_vert = i*width+1; (current_vert <= (i+1)*width) && (current_vert <= height*width+1); current_vert++) {
             ret += std::to_string(current_vert) + " " + (i > 0 ? std::to_string(width) : "1") + "\n";
             for (int prev_vert = i*width; (prev_vert >= 0) && (prev_vert >= (i-1)*width+1); prev_vert--) {
                 double df_coef = rand_df_coef(max_factor, prob_of_small);
-                ret += std::to_string(prev_vert) + " " + std::to_string(df_coef) + "\n";
+                ret += "P " + std::to_string(prev_vert) + " " + std::to_string(df_coef) + "\n";
             }
         }
     }
+    ret += "O 0 1\n";
+    ret += "G " + std::to_string(height*2 + 1) + " 1\n";
     return ret;
 }
 
@@ -87,27 +97,38 @@ void add_edge(Vertex* from, Vertex* to, int max_factor, double prob_of_small) {
     to->incoming.insert(make_pair(from, df_coef));
 }
 
-RegGraph gen_rand_dag(int num_vert, int sdata, double edge_prob, int max_factor, double prob_of_small) {
-    map<int, Vertex*> type_to_vert;
+RegGraph gen_rand_dag(int num_vert, int num_gbks, int sdata, double edge_prob, int max_factor, double prob_of_small) {
+    vector<Vertex*> type_to_vert;
+    Vertex* input = new Vertex(0, "I");
+    type_to_vert.push_back(input);
+    int step = num_vert/num_gbks;
     for (int i = 1; i <= num_vert; i++) {
-        Vertex* v1 = get_vert_of_type(i, type_to_vert);
+        Vertex* vert = NULL;
+        if ((i + step/2)%step == 0) {
+            vert = new Vertex(i, "G");
+        } else {
+            vert = new Vertex(i, "P");
+        }
+        type_to_vert.push_back(vert);
+    }
+    Vertex* output = new Vertex(num_vert+1, "O");
+    type_to_vert.push_back(output);
+    for (int i = 1; i <= num_vert; i++) {
+        Vertex* v1 = type_to_vert[i];
         if (v1->incoming.size() == 0) {
-            Vertex* root = get_vert_of_type(0, type_to_vert);
-            add_edge(root, v1, max_factor, prob_of_small);
+            add_edge(input, v1, max_factor, prob_of_small);
         }
         for (int j = i+1; j <= num_vert; j++) {
             if (rand01() < edge_prob) {
-                Vertex* v2 = get_vert_of_type(j, type_to_vert);
+                Vertex* v2 = type_to_vert[j];
                 add_edge(v1, v2, max_factor, prob_of_small);
             }
         }
     }
     set<Vertex*> vertices;
-    Vertex* sink = get_vert_of_type(num_vert+1, type_to_vert);
-    for (auto it : type_to_vert) {
-        Vertex* v = it.second;
-        if ((v->outgoing.size() == 0) && (v != sink)) {
-            add_edge(v, sink, max_factor, prob_of_small);
+    for (Vertex* v : type_to_vert) {
+        if ((v->outgoing.size() == 0) && (v != output)) {
+            add_edge(v, output, max_factor, prob_of_small);
         }
         vertices.insert(v);
     }
@@ -128,10 +149,10 @@ int main(int argc, char** argv) {
         cin >> height >> width >> sdata >> max_factor >> prob_of_small;
         cout << gen_rand_entwined(height, width, sdata, max_factor, prob_of_small);
     } else if(type == "dag") {
-        int num_vertices, sdata, max_factor;
+        int num_vertices, sdata, max_factor, num_gbk;
         double edge_prob, prob_of_small;
-        cin >> num_vertices >> sdata >> edge_prob >> max_factor >> prob_of_small;
-        cout << gen_rand_dag(num_vertices, sdata, edge_prob, max_factor, prob_of_small).to_string();
+        cin >> num_vertices >> num_gbk >> sdata >> edge_prob >> max_factor >> prob_of_small;
+        cout << gen_rand_dag(num_vertices, num_gbk, sdata, edge_prob, max_factor, prob_of_small).to_string();
     } else {
         cout << "Unknown type: " << type << endl;
     }
