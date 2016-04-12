c38d01e823513125103cfb0aa68057e32048b538
Juraj Machac <jurajmchc@gmail.com>
2016-04-12 11:50:45 +0200

    Added low & high ranges for df coefficients

--- graph_gen.cpp
+++ graph_gen.cpp
@@ -54,14 +54,14 @@ double rand01() {
     return ((double)rand())/RAND_MAX;
 }
 
-double rand_df_coef(int max_factor, double prob_of_small) {
+double rand_df_coef(double low, double high, double prob_of_small) {
     double norm = rand01();
-    double on_interval = norm + (((double)1)/max_factor)*(1 - norm);
+    double on_interval = norm*(high - low) + low;
     bool should_invert = rand01() > prob_of_small;
     return (should_invert ? 1/on_interval : on_interval);
 }
 
-RegGraph gen_rand_entwined(int height, int width, int sdata, int max_factor, double prob_of_small) {
+RegGraph gen_rand_entwined(int height, int width, int sdata, double low, double high, double prob_of_small) {
     Vertex* input = new Vertex(0, "I");
     Vertex* source = new Vertex(1, "G");
     vector<Vertex*> vertices;
@@ -74,7 +74,7 @@ RegGraph gen_rand_entwined(int height, int width, int sdata, int max_factor, dou
             Vertex* vertex = new Vertex(current_vert, current_vert == height*width+2 ? "G" : "P");
             vertices.push_back(vertex);
             for (int prev_vert = i*width+1; (prev_vert >= 1) && (prev_vert > (i-1)*width+1); prev_vert--) {
-                double df_coef = rand_df_coef(max_factor, prob_of_small);
+                double df_coef = rand_df_coef(low, high, prob_of_small);
                 vertex->incoming.insert(make_pair(vertices[prev_vert], df_coef));
                 vertices[prev_vert]->outgoing.insert(make_pair(vertex, df_coef));
             }
@@ -99,13 +99,13 @@ Vertex* get_vert_of_type(int type, map<int, Vertex*> &type_to_vert) {
         return type_to_vert[type];
     }
 }
-void add_edge(Vertex* from, Vertex* to, int max_factor, double prob_of_small) {
-    double df_coef = rand_df_coef(max_factor, prob_of_small);
+void add_edge(Vertex* from, Vertex* to, double low, double high, double prob_of_small) {
+    double df_coef = rand_df_coef(low, high, prob_of_small);
     from->outgoing.insert(make_pair(to, df_coef));
     to->incoming.insert(make_pair(from, df_coef));
 }
 
-RegGraph gen_rand_dag(int num_vert, int num_gbks, int sdata, double edge_prob, int max_factor, double prob_of_small) {
+RegGraph gen_rand_dag(int num_vert, int num_gbks, int sdata, double edge_prob, double low, double high, double prob_of_small) {
     vector<Vertex*> type_to_vert;
     Vertex* input = new Vertex(0, "I");
     type_to_vert.push_back(input);
@@ -124,19 +124,19 @@ RegGraph gen_rand_dag(int num_vert, int num_gbks, int sdata, double edge_prob, i
     for (int i = 1; i <= num_vert; i++) {
         Vertex* v1 = type_to_vert[i];
         if (v1->incoming.size() == 0) {
-            add_edge(input, v1, max_factor, prob_of_small);
+            add_edge(input, v1, low, high, prob_of_small);
         }
         for (int j = i+1; j <= num_vert; j++) {
             if (rand01() < edge_prob) {
                 Vertex* v2 = type_to_vert[j];
-                add_edge(v1, v2, max_factor, prob_of_small);
+                add_edge(v1, v2, low, high, prob_of_small);
             }
         }
     }
     set<Vertex*> vertices;
     for (Vertex* v : type_to_vert) {
         if ((v->outgoing.size() == 0) && (v != output)) {
-            add_edge(v, output, max_factor, prob_of_small);
+            add_edge(v, output, low, high, prob_of_small);
         }
         vertices.insert(v);
     }
@@ -152,15 +152,15 @@ int main(int argc, char** argv) {
     cin >> type;
     
     if (type == "entwined") {
-        int height, width, sdata, max_factor;
-        double prob_of_small;
-        cin >> height >> width >> sdata >> max_factor >> prob_of_small;
-        cout << gen_rand_entwined(height, width, sdata, max_factor, prob_of_small).to_string();
+        int height, width, sdata;
+        double prob_of_small, low, high;
+        cin >> height >> width >> sdata >> low >> high >> prob_of_small;
+        cout << gen_rand_entwined(height, width, sdata, low, high, prob_of_small).to_string();
     } else if(type == "dag") {
-        int num_vertices, sdata, max_factor, num_gbk;
-        double edge_prob, prob_of_small;
-        cin >> num_vertices >> num_gbk >> sdata >> edge_prob >> max_factor >> prob_of_small;
-        cout << gen_rand_dag(num_vertices, num_gbk, sdata, edge_prob, max_factor, prob_of_small).to_string();
+        int num_vertices, sdata, num_gbk;
+        double edge_prob, prob_of_small, low, high;
+        cin >> num_vertices >> num_gbk >> sdata >> edge_prob >> low >> high >> prob_of_small;
+        cout << gen_rand_dag(num_vertices, num_gbk, sdata, edge_prob, low, high, prob_of_small).to_string();
     } else {
         cout << "Unknown type: " << type << endl;
     }
